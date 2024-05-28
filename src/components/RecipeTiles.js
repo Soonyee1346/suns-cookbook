2c239beaad6efd2fb238d830ebbc5f64d484c531
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-05-28 15:11:17 +1000

    finished Recipes page

--- src/components/RecipeTiles.js
+++ src/components/RecipeTiles.js
@@ -12,12 +12,10 @@ function RecipeTiles(props) {
             const tile = recipeArr[i];
             const img = `/images/${tile.img}`
             const key = `tile${i}`
-            console.log(img)
             row = (
                 <Link className="box" to="/Recipes/Spaghetti.html" key={key}>
                         <img src={img} />
                         <h1 className="boxtitle bold">{tile.name}</h1>
-                        <h1 className="boxprice bold">{tile.totalPrice}</h1>
                 </Link>
             );
             rows.push(row);
@@ -25,11 +23,10 @@ function RecipeTiles(props) {
     
         return rows;
     
-    
     }
 
     return (
-        <>{createTiles(props.recipeArr)}</>
+        <>{createTiles(props.recipes)}</>
     )
 }
 
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,73 +1,13 @@
 import Tiles from "../components/RecipeTiles"
 
 
-function Recipes() {
-
-    const spaghetti = {
-        name: "Spaghetti",
-        img: "spaghetti.jpeg",
-        ingredients: [{
-            name: "Pasta",
-            rrp: 3,
-            price: 2
-        }, {
-            name: "Minced Meat",
-            rrp: 5,
-            price: 5
-        }, {
-            name: "Pasta Sauce",
-            rrp: 4,
-            price: 3
-        }, {
-            name: "Garlic",
-            rrp: 2,
-            price: 2
-        }, {
-            name: "Onion",
-            rrp: 1.5,
-            price: 1.5
-        }],
-        get totalPrice() {
-            return this.ingredients.reduce((total, ingredient) => total + ingredient.price, 0).toFixed(2);
-        }
-    }
-
-    const chickenRice = {
-        name: "Chicken Rice",
-        img: "chickenrice.jpg",
-        ingredients: [{
-            name: "Pasta",
-            rrp: 3,
-            price: 2
-        }, {
-            name: "Minced Meat",
-            rrp: 5,
-            price: 5
-        }, {
-            name: "Pasta Sauce",
-            rrp: 4,
-            price: 3
-        }, {
-            name: "Garlic",
-            rrp: 2,
-            price: 2
-        }, {
-            name: "Onion",
-            rrp: 1.5,
-            price: 1.5
-        }],
-        get totalPrice() {
-            return this.ingredients.reduce((total, ingredient) => total + ingredient.price, 0).toFixed(2);
-        }
-    }
-
-    const recipeArr = [spaghetti, chickenRice, spaghetti]
+function Recipes(props) {
 
     return (
         <>
             <h1>Recipes</h1>
             <div className="recipes">
-                <Tiles recipeArr={recipeArr}/>
+                <Tiles recipes={props.Data}/>
             </div>
         </>
     )
