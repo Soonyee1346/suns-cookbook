8643338051c005e63a0b0919f48b9bc1ab2c4122
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-05-29 11:39:27 +1000

    working on RecipeMaker

--- src/pages/RecipeMaker.js
+++ src/pages/RecipeMaker.js
@@ -1,7 +1,35 @@
-
 function RecipeMaker() {
+
+    function addIng() {
+        var ingList = document.getElementById("ingList");
+
+        ingList.appendChild() //idk about this method bro
+    }
+
     return (
-        <h1>RecipeMaker</h1>
+        <>
+            <h1>RecipeMaker</h1>
+            <div className="container">
+                <div className="name section">
+                    <h3>Recipe Name</h3>
+                    <input type="text" id="name" value="Recipe Name"/>
+                </div>
+                <div className="image section">
+                    <h3>Recipe Image</h3>
+                    <input type="file" id="image"/>
+                </div>
+                <div className="ingredients section">
+                    <h3>Ingredients</h3>
+                    <div id="ingList">
+                        <div id="ing1">
+                            <input type="text" id="ingredient1" value="ingredient"/>
+                            <input type="text" id="quantity1" value="quantity"/>
+                        </div>
+                    </div>
+                </div>
+                <button className="addIng"><i class="fa-solid fa-plus" onClick={addIng()}></i></button>
+            </div>
+        </>
     )
 }
 
