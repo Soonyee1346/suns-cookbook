2a5fa97655a0fe56726a92e210d41ef8daa4099b
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-07-17 11:16:19 +1000

    css and refresh button prep

--- src/pages/Recipe/RecipeTemplate.js
+++ src/pages/Recipe/RecipeTemplate.js
@@ -1,4 +1,5 @@
 import RecipePage from "../../components/RecipePage"
+import '../css/recipePage.css';
 
 function Recipe(props) {
 
@@ -7,6 +8,9 @@ function Recipe(props) {
             <h1>{props.Recipe.name}</h1>
             <div className="recipes">
                 <RecipePage recipe={props.Recipe}/>
+            </div>
+            <div className="buttons">
+                <button className="refreshHome" onClick={changeRecipes}><i className="fa fa-refresh" aria-hidden="true"></i></button>
             </div>
         </>
     )
