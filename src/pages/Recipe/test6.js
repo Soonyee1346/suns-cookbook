0e7e616d0fea449b7749cc031be73b60ca71ba3a
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-07-30 11:46:38 +1000

    Update test6.js

--- src/pages/Recipe/test6.js
+++ src/pages/Recipe/test6.js
@@ -2,20 +2,30 @@ import React from 'react';
 import RecipePage from "../../components/RecipePage";
 import '../../css/recipePage.css';
 import deleteRecipe from "../../api/deleteRecipe"; // Import as default
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 function Recipe(props) {
+
+    const navigate = useNavigate();
+    const [shouldNavigate, setShouldNavigate] = useState(false);
 
     const recipeId = props.Recipe ? props.Recipe.id : '3';
 
     const deleteEvent = async () => {
         try {
             await deleteRecipe(props.Recipe.id);
-            
+            setShouldNavigate(true);
         } catch (error) {
             console.error('Error deleting recipe:', error);
         }
     };
+
+    useEffect(() => {
+        if (shouldNavigate) {
+            navigate('/Recipes');
+        }
+    }, [shouldNavigate, navigate]);
 
     return (
         <>
