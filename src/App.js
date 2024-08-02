6e5fbe21970fb17c4956dbb83a3175c7d749aade
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-08-02 11:47:16 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Test5 from './pages/Recipe/Test5.js';
 import Test4 from './pages/Recipe/Test4.js';
 import Test from './pages/Recipe/Test.js';
 import ChickenRice from './pages/Recipe/ChickenRice.js';
-import HoneyChicken from './pages/Recipe/HoneyChickenRiceandPotato.js';
+import HoneyChickenRiceandPotato from './pages/Recipe/HoneyChickenRiceandPotato.js';
 import Spaghetti from './pages/Recipe/Spaghetti.js';
 import './css/App.css';
 import Nav from './components/Nav.js';
@@ -50,7 +50,7 @@ function App() {
         <Route path="/Recipes/Test5" element={<Test5 Recipe={Data.length > 0 ? Data[0].recipes[getRecipeIndex(6)] : []}/>} />
         <Route path="/Recipes/Test4" element={<Test4 Recipe={Data.length > 0 ? Data[0].recipes[getRecipeIndex(5)] : []}/>} />
         <Route path="/Recipes/Test" element={<Test Recipe={Data.length > 0 ? Data[0].recipes[getRecipeIndex(4)] : []}/>} />
-        <Route path="/Recipes/HoneyChickenRiceandPotato" element={<HoneyChicken Recipe={Data.length > 0 ? Data[0].recipes[getRecipeIndex(3)] : []}/>} />
+        <Route path="/Recipes/HoneyChickenRiceandPotato" element={<HoneyChickenRiceandPotato Recipe={Data.length > 0 ? Data[0].recipes[getRecipeIndex(3)] : []}/>} />
         <Route path="/Recipes/ChickenRice" element={<ChickenRice Recipe={Data.length > 0 ? Data[0].recipes[getRecipeIndex(2)] : []}/>} />
         <Route path="/Recipes/Spaghetti" element={<Spaghetti Recipe={Data.length > 0 ? Data[0].recipes[getRecipeIndex(1)] : []}/>} />
         <Route path="/" element={<Home Recipes={Data.length > 0 ? Data[0].recipes : []} />}/>
